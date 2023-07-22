399e962496c4dfc68b9e25a094ce9627895399f0
naskya <m@naskya.net>
2023-07-22 11:25:52 +0000

    hotfix: handle migration failure

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+function copy_missing_file {
+  [ -f "packages/backend/native-utils/built/index.js" ] \
+  || cp packages/index.js packages/backend/native-utils/built/index.js
+}
+
 git checkout -- package.json packages/backend/assets
 git pull --ff
 
@@ -7,5 +12,6 @@ sed -i -r "s/\"version\": \"([^+]+).*\",$/\"version\": \"\\1+neko:${COMMIT}\",/"
 
 corepack prepare pnpm@latest --activate
 pnpm i
-NODE_ENV=production pnpm run rebuild
-pnpm run migrate
+NODE_ENV=production pnpm run rebuild \
+&& copy_missing_file \
+&& pnpm run migrate
