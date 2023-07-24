c16fbc72f78cab35b1febe52f1a684ab91c7d24f
naskya <m@naskya.net>
2023-07-24 03:55:00 +0000

    chore: update update.sh (&& everithing to be safe)

--- update.sh
+++ update.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 function copy_missing_file {
-  [ -f "packages/backend/native-utils/built/index.js" ] \
-  || cp packages/index.js packages/backend/native-utils/built/index.js
+  [ -f "packages/backend/native-utils/built/index.js" ] || \
+  cp packages/index.js packages/backend/native-utils/built/index.js
 }
 
-git checkout -- package.json packages/backend/assets
-git pull --ff
-
-COMMIT=$(git rev-parse --short HEAD)
-sed -i -r "s/\"version\": \"([^+]+).*\",$/\"version\": \"\\1+neko:${COMMIT}\",/" package.json
-
-corepack prepare pnpm@latest --activate
-pnpm i
-NODE_ENV=production pnpm run rebuild \
-&& copy_missing_file \
-&& pnpm run migrate
+git checkout -- package.json packages/backend/assets && \
+git pull --ff && \
+COMMIT=$(git rev-parse --short HEAD) && \
+sed -i -r "s/\"version\": \"([^+]+).*\",$/\"version\": \"\\1+neko:${COMMIT}\",/" package.json && \
+corepack prepare pnpm@latest --activate && \
+pnpm i && \
+NODE_ENV=production pnpm run rebuild && \
+copy_missing_file && \
+pnpm run migrate
