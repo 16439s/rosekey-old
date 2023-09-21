d0b8984d0e050eddcc5cb6019c86383dff15b426
naskya <m@naskya.net>
2023-09-21 17:19:20 +0900

    fix: get latest version from the forked repo (close #54)

--- packages/backend/src/server/api/endpoints/latest-version.ts
+++ packages/backend/src/server/api/endpoints/latest-version.ts
@@ -15,10 +15,12 @@ export const paramDef = {
 
 export default define(meta, paramDef, async () => {
 	let tag_name;
-	await fetch("https://git.joinfirefish.org/api/v4/projects/7/releases")
+	await fetch(
+		"https://code.naskya.net/api/v1/repos/naskya/firefish/tags?page=1&limit=1",
+	)
 		.then((response) => response.json())
 		.then((data) => {
-			tag_name = data[0].tag_name;
+			tag_name = data[0].name;
 		});
 
 	return {
