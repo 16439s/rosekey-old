65817a540f62dd9cb237111e21ec2d1d644598e4
naskya <m@naskya.net>
2023-10-30 19:21:02 +0900

    chore: remove unnecessary whitespace

--- packages/backend/migration-neko/1698420787202-pgroonga.js
+++ packages/backend/migration-neko/1698420787202-pgroonga.js
@@ -9,7 +9,7 @@ export class Pgroonga1698420787202 {
 			`CREATE INDEX "IDX_065d4d8f3b5adb4a08841eae3c" ON "user" USING "pgroonga" ("name" pgroonga_varchar_full_text_search_ops_v2)`,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_fcb770976ff8240af5799e3ffc" ON "user_profile" USING "pgroonga" ("description" pgroonga_varchar_full_text_search_ops_v2) `,
+			`CREATE INDEX "IDX_fcb770976ff8240af5799e3ffc" ON "user_profile" USING "pgroonga" ("description" pgroonga_varchar_full_text_search_ops_v2)`,
 		);
 	}
 
