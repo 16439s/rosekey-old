4c445bf5905642a75d4e5920a71aa638144494f9
naskya <m@naskya.net>
2023-11-17 16:25:39 +0900

    chore: add a message to tell that docker image build takes time

--- neko/update/docker.sh
+++ neko/update/docker.sh
@@ -11,6 +11,8 @@ pull() {
 if ! pull; then
   say "awawa, the image may not be compatible with your environment..."
   say "Gonnya try building the image locally!"
+
+  say "It takes some time! Why not brew a cup of cofe?"
   docker build --tag docker.io/naskya/firefish --build-arg VERSION="$(version)" .
 fi
 
