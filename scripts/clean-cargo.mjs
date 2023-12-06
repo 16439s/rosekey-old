f0d82ebb945ba7b231bd9e234a03f199edf87932
naskya <m@naskya.net>
2023-12-06 23:58:52 +0900

    dev: add clean-npm and clean-cargo, rename clean to clean-built

---
+++ scripts/clean-cargo.mjs
@@ -0,0 +1,13 @@
+import fs from "node:fs";
+import path, { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { execa } from "execa";
+
+(async () => {
+	const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+	execa("cargo", ["clean"], {
+		cwd: join(__dirname, "/../packages/backend/native-utils"),
+		stdio: "inherit",
+	});
+})();
